ce7047849929e185bbc5203a61af76ef2119a989
zhuweiming-1 <15123001642@163.com>
2026-02-13 11:42:47 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,13 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+		int sum = 0;
+        while (x < 10) {
+            System.out.print(x + " ");
+            x = x + 1;
+			sum = sum + x;
+			System.oyt.print(sum);
+        }
+		System.oyt.println();
+    }
+}
